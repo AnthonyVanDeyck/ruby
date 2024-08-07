ce327726597fbf02a346be146f49c1f7ad2cbb74
AnthonyVanDeyck <anthonyvandeyck@gmail.com>
2024-08-07 12:58:49 +0200

    new fonction

--- test01/test02.rb
+++ test01/test02.rb
@@ -1,3 +1,6 @@
 puts "merci de rentrez votre nom"
 nom = gets.chomp
 puts "Bonjour #{nom} ceci est un test :)"
+puts "introduisez un chiffre"
+chiffre = gets.chomp
+puts chiffre.to_i.even?  # le .to_i transforme un char en int
